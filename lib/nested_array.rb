d5bdb966308ae453ab73ae0c84eb0b267dd02992
Robert Phillips <rpdecks@gmail.com>
2020-03-13 15:19:38 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -40,9 +40,8 @@ def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
   
-  matrix
+  matrix[row][column]
   binding.pry
-  #matrix[row][column]
   
 end
 
